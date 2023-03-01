04bb6b5417cfc2b794e2a1ca973e40a94a55641c
webstar <webstar0103@gmail.com>
2023-03-02 02:02:42 +0900

    feat: add s_and_r page first commit

---
+++ resources/views/S_AND_R.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+@section('title') Gainloe - FAQ'S @endsection
+@section('keywords') Home,About,Contact,Car @endsection
+@section('description') Write some descripton about the webpage @endsection
+@section('content')
